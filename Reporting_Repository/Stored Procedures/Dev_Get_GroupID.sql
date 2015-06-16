5a1b614ad324096fb7acda1e3f743277fef1f28b
simoncho <simon@simonsql.com>
2015-06-16 15:47:06 -0700

    update

---
+++ Reporting_Repository/Stored Procedures/Dev_Get_GroupID.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE Dev_Get_GroupID
+AS
+SELECT GroupID, P.ProjectName
+  FROM [$(TFS_Server)].[$(TFS_Database)].[dbo].[tbl_BuildGroup] G WITH(NOLOCK)
+  JOIN [$(TFS_Server)].[$(TFS_Database)].[dbo].[tbl_Project] P WITH(NOLOCK)
+    ON G.TeamProject = P.ProjectUri AND g.PartitionId = p.PartitionId
